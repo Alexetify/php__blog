25235ddc4883ecc17ff808ceaf22a4e25812f49b
ALEXEI-PC\Alexei-PC <hhmh@rambler.ru>
2017-02-16 16:48:14 +0200

    Menu

--- admin/index.php
+++ admin/index.php
@@ -12,7 +12,14 @@
        <link rel="stylesheet" type="text/css" href="styles/main.css" />
    </head>
    <body>
-       
+       <h1 id="header">Admin-панель</h1>
+       <h2>Меню</h2>
+       <p>
+           <a href="#">Добавить новую статью</a>
+       </p>
+       <p>
+           <a href="../logout.php">Выход</a>
+       </p>
    </body>
     
 </html>
